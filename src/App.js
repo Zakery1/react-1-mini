aba3dc925afcab1f6ef92e90e2ab9c46b025de31
Zachary Graham <zakgraham@gmail.com>
2018-06-22 14:53:20 -0700

    add friend list

--- src/App.js
+++ src/App.js
@@ -2,9 +2,52 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      picture:'picture url',
+      name: 'Eric',
+      friends: [{
+        name: 'My 404 friend',
+        picture: 'https://http.cat/404'
+      }, {
+        name: 'My chill cat with the munchies',
+        picture: 'https://http.cat/420'
+      }]
+    };
+  }
+
+
+  changeName(name) {
+    this.setState({
+      name: name,
+    })
+  }
+
+  changePicture(picture){
+    this.setState({
+      picture: picture,
+    })
+  }
+
+
   render() {
     return (
-      <div></div>
+      <div>
+        Name: <input value={this.state.name} onChange={(event)=> this.changeName(event.target.value)} />
+        Picture: <input value={this.state.picture} onChange={(event)=> this.changePicture(event.target.value)} />
+        <div>
+          {this.state.friends.map(friend => {
+            return (
+              <div>
+                <img src={friend.picture}/>
+                <b>{friend.name}</b>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+      
     );
   }
 }
